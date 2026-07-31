0630fb29cf65332daea8d99a9b9a95467b752dac
looft <tr11ck55terr@gmail.com>
2026-07-31 11:20:21 +0500

    Добавил обработку исключейни try и except для большей стабильности кода и понятного вывода ошибок

--- renamer/main.py
+++ renamer/main.py
@@ -1,19 +1,27 @@
 from pathlib import Path
-from func_rename import new_name
 import random
 import sys
 
 def main():
-    _, path, file_name = sys.argv
-    path = Path(path)
+    try:
+        _, path, file_name = sys.argv
+        path = Path(path)
+    except ValueError:
+        print("Not enough values")
+        return
     if path.is_dir():
         for item_path in path.iterdir():
-            suff = item_path.suffix
-            ran_count = random.randint(0, 10000)
-            new_path = path / f"{file_name}{ran_count}{suff}"
-            item_path.rename(new_path)
+            try:
+                suff = item_path.suffix
+                ran_count = random.randint(0, 10000)
+                new_path = path / f"{file_name}{ran_count}{suff}"
+                item_path.rename(new_path)
+            except FileNotFoundError:
+                print("File Not Found")
+            except FileExistsError:
+                print("File Exists")
     else:
-        print('Incorrect PATH')
-
+        print("Incorrect PATH")
+        
 if __name__ == "__main__":
     main()
